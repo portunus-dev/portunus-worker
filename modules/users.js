de57a1372573005b025f40995b795477dae02d76
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2021-11-23 15:02:37 -0500

    user - update() function to update both deta.Base and KV user record

--- modules/users.js
+++ modules/users.js
@@ -1,9 +1,19 @@
 const deta = require('./db')
 
-// deta Base - users
-// TODO: put/update
+// deta Base - users, for UI use
 module.exports.get = (email) =>
   deta.Base('users').fetch({ email }, { limit: 1 })
 
-// Cloudflare Workers KV - USERS
+module.exports.update = (user) => {
+  if (!user.key) {
+    throw new Error('user.key is required')
+  }
+  return Promise.all([
+    // TODO: do this "transactionally"
+    deta.Base('users').put(user), // deta.Base put(data)
+    USERS.put(user.email, user), // KV put(key, value)
+  ])
+}
+
+// Cloudflare Workers KV - USERS, for CLI use
 module.exports.getKVUser = (email) => USERS.get(email, { type: 'json' })
