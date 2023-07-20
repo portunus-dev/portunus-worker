4662d74b1ffe78b8451d978289e59e90a02d5d0f
ashishjullia19 <ashishjullia19@gmail.com>
2023-07-20 15:11:31 -0400

    users-email - Lowercase the email value on CRUD operations for deta and USERS kv

--- handlers.js
+++ handlers.js
@@ -421,7 +421,7 @@ module.exports.addUserToTeam = async ({
       throw new HTTPError('Invalid access: team admin required', 403)
     }
 
-    let kvUser = await USERS.get(userEmail, { type: 'json' })
+    let kvUser = await USERS.get(userEmail.toLowerCase(), { type: 'json' })
 
     if (!kvUser) {
       kvUser = await createUser(userEmail, { getKVUser: true })
@@ -452,7 +452,7 @@ module.exports.removeUserFromTeam = async ({
       throw new HTTPError('Invalid access: team admin required', 403)
     }
 
-    const kvUser = await USERS.get(userEmail, { type: 'json' })
+    const kvUser = await USERS.get(userEmail.toLowerCase(), { type: 'json' })
 
     if (!kvUser) {
       throw new HTTPError('Invalid user: user not found', 400)
@@ -483,7 +483,7 @@ module.exports.addUserToAdmin = async ({
       throw new HTTPError('Invalid access: team admin required', 403)
     }
 
-    const kvUser = await USERS.get(userEmail, { type: 'json' })
+    const kvUser = await USERS.get(userEmail.toLowerCase(), { type: 'json' })
 
     if (!kvUser) {
       throw new HTTPError('Invalid user: user not found', 400)
@@ -514,7 +514,7 @@ module.exports.removeUserFromAdmin = async ({
       throw new HTTPError('Invalid access: team admin required', 403)
     }
 
-    const kvUser = await USERS.get(userEmail, { type: 'json' })
+    const kvUser = await USERS.get(userEmail.toLowerCase(), { type: 'json' })
 
     if (!kvUser) {
       throw new HTTPError('Invalid user: user not found', 400)
